86596491621c5ed5e5d146583b5ffe245aede884
lerquis <estebanrojasquesada@gmail.com>
2025-06-12 18:45:16 -0600

    Saving astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
@@ -10,7 +9,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
-  adapter: vercel(),
+  adapter: vercel({
+    isr: true,
+  }),
+  output: "server",
 });
