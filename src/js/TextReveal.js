2daf510d5b5fcca5e0db985da3b84d17ae2c0be8
lerquis <estebanrojasquesada@gmail.com>
2025-06-27 18:44:42 -0600

    Adding animations, text, statistics, and titles

---
+++ src/js/TextReveal.js
@@ -0,0 +1,42 @@
+import SplitType from "split-type";
+import Observer from "./Observer";
+import gsap from "gsap";
+
+export default class TextReveal extends Observer {
+  constructor({ element }) {
+    super({ element });
+
+    this.element = element;
+    this.splitDivs = new SplitType(element, {
+      types: "words",
+    });
+
+    this.splitWords = new SplitType(this.splitDivs.words, {
+      types: "words",
+    });
+
+    this._setInitials();
+  }
+
+  _setInitials() {
+    gsap.set(this.splitDivs.words, {
+      overflow: "hidden",
+    });
+    gsap.set(this.splitWords.words, {
+      yPercent: 100,
+      opacity: 0,
+    });
+  }
+
+  onEnter() {
+    gsap.to(this.splitWords.words, {
+      duration: 1,
+      opacity: 1,
+      yPercent: 0,
+      stagger: 0.03,
+      ease: "power3.out",
+    });
+  }
+
+  onLeave() {}
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,18 @@
 import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import TextReveal from "./TextReveal";
+
+gsap.registerPlugin(ScrollTrigger);
 
 class App {
   constructor() {
     this._createLenis();
     this._render();
     this._backToTop();
+    this._textsToReveal();
+    this._statisticsAnimation();
+    this._titlesAnimation();
   }
 
   _createLenis() {
@@ -22,6 +30,68 @@ class App {
     const button = document.querySelector(".back_to_top");
     button.addEventListener("click", () => {
       this.lenis.scrollTo(0);
+    });
+  }
+
+  _textsToReveal() {
+    // Seleccionar todos los elementos con la clase text-animation
+    const textElements = document.querySelectorAll(".text-animation");
+    textElements.forEach((element) => new TextReveal({ element }));
+  }
+
+  _statisticsAnimation() {
+    // Esperar a que GSAP esté disponible
+    document.addEventListener("DOMContentLoaded", async () => {
+      // Si GSAP se carga de forma asíncrona
+      const { gsap } = await import("gsap");
+      const { ScrollTrigger } = await import("gsap/ScrollTrigger");
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      const elements = document.querySelectorAll(".estadistica");
+      if (elements.length > 0) {
+        elements.forEach((el) => {
+          const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: el,
+              start: "top center",
+              end: "bottom center",
+              toggleActions: "play none none reverse",
+              scrub: true,
+              once: true,
+            },
+          });
+
+          tl.fromTo(el, { opacity: 0 }, { opacity: 1, duration: 1 });
+        });
+      }
+    });
+  }
+
+  _titlesAnimation() {
+    document.addEventListener("DOMContentLoaded", async () => {
+      // Si GSAP se carga de forma asíncrona
+      const { gsap } = await import("gsap");
+      const { ScrollTrigger } = await import("gsap/ScrollTrigger");
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      const titles = document.querySelectorAll(".title-animation");
+      if (titles.length > 0) {
+        titles.forEach((el) => {
+          const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: el,
+              start: "top 80%",
+              toggleActions: "play none none reverse",
+              scrub: true,
+              once: true,
+            },
+          });
+
+          tl.fromTo(el, { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+        });
+      }
     });
   }
 }
